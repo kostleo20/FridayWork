9dece47f9c7ccadb5006cf732e41864158e44b16
kostleo20 <leo.kostin12@gmail.com>
2021-06-15 20:54:12 -0700

    added plugins & updated pom.xml

--- src/test/java/CILabTest.java
+++ src/test/java/CILabTest.java
@@ -2,7 +2,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CILabTest {
 
@@ -20,13 +20,19 @@ public class CILabTest {
 
     @Test
     public void detectCapitalUseTest1() {
-       fail("Not yet implemented");
-
+        myString.setString("USA");
+        assertFalse(myString.detectCapitalUse());
     }
+
     @Test
     public void detectCapitalUseTest2() {
-        fail("Not yet implemented");
+       myString.setString("Fla6");
+       assertFalse(myString.detectCapitalUse());
     }
 
-
+    @Test
+    public void detectCapitalUseTest3() {
+        myString.setString("softwareengineering");
+        assertTrue(myString.detectCapitalUse());
+    }
 }
